021b8a5871556a428dfd03c7eb6ecad4b73737f2
Rakesh Mondal <mondalrakesh0540@gmail.com>
2026-08-25 11:48:56 +0530

    Integrate popular MyInstants meme soundboard audio clips for study alerts

--- src/utils/audioManager.js
+++ src/utils/audioManager.js
@@ -2,12 +2,12 @@
 // FocusGuard AI Alert Sound & Funny Student Meme Manager
 
 const AUDIO_FILES = {
-  'start-study': ['/audio/start-study.wav', '/audio/start-study.mp3'],
-  'tab-change': ['/audio/tab-change.wav', '/audio/tab-change.mp3'],
-  'face-missing': ['/audio/face-missing.wav', '/audio/face-missing.mp3'],
-  'distracted': ['/audio/distracted.wav', '/audio/distracted.mp3'],
-  'back-to-study': ['/audio/back-to-study.wav', '/audio/back-to-study.mp3'],
-  'sleep-warning': ['/audio/sleep-warning.wav', '/audio/sleep-warning.mp3'],
+  'start-study': ['/audio/start-study.mp3', '/audio/start-study.wav', 'https://www.myinstants.com/media/sounds/anime-wow-sound-effect.mp3'],
+  'tab-change': ['/audio/tab-change.mp3', '/audio/tab-change.wav', 'https://www.myinstants.com/media/sounds/vine-boom.mp3'],
+  'face-missing': ['/audio/face-missing.mp3', '/audio/face-missing.wav', 'https://www.myinstants.com/media/sounds/asian-meme-huh.mp3'],
+  'distracted': ['/audio/distracted.mp3', '/audio/distracted.wav', 'https://www.myinstants.com/media/sounds/snore-mimimimimimi.mp3'],
+  'back-to-study': ['/audio/back-to-study.mp3', '/audio/back-to-study.wav', 'https://www.myinstants.com/media/sounds/correct.mp3'],
+  'sleep-warning': ['/audio/sleep-warning.mp3', '/audio/sleep-warning.wav', 'https://www.myinstants.com/media/sounds/loud-alarm-clock.mp3'],
 };
 
 export const FUNNY_MEMES = {
